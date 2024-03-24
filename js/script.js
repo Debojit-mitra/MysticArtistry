452b26b19ce8e894237fdeb2bce5069901194266
Debojit Mitra <debojit16mitra@gmail.com>
2024-03-24 23:16:40 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function openLoginPage() {
   // Open loginpage.html in a new tab or window
-  window.open("loginpage.html", "_blank");
+  window.open("login.html", "_self");
+}
+
+function openShopPage() {
+  // Open showPage.html in a new tab or window
+  window.open("index.html", "_self");
 }
