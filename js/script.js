b21b431aa0ac13f4fdac70b814d30c51dc3952fe
Debojit Mitra <debojit16mitra@gmail.com>
2024-04-13 18:34:15 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ function openLoginPage() {
   window.open("login.html", "_self");
 }
 
+function openAboutPage() {
+  // Open loginpage.html in a new tab or window
+  window.open("about.html", "_self");
+}
+
 function openShopPage() {
   // Open showPage.html in a new tab or window
   window.open("index.html", "_self");
@@ -23,54 +28,3 @@ function toggleMenu() {
     });
   }
 }
-
-const slides = document.querySelectorAll(".slide");
-const indicators = document.querySelectorAll(".indicator");
-
-let index = 0;
-
-const updateIndicator = () => {
-  indicators.forEach((indicator) => indicator.classList.remove("active"));
-  indicators[index].classList.add("active");
-};
-
-const goToSlide = () => {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
-  });
-};
-
-const nextSlide = () => {
-  index = (index + 1) % slides.length;
-  updateIndicator();
-  goToSlide();
-};
-
-const prevSlide = () => {
-  index = (index - 1 + slides.length) % slides.length;
-  updateIndicator();
-  goToSlide();
-};
-
-indicators.forEach((indicator, i) => {
-  indicator.addEventListener("click", () => {
-    index = i;
-    updateIndicator();
-    goToSlide();
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  updateIndicator();
-  goToSlide();
-});
-
-document.querySelector(".slider").addEventListener("wheel", (event) => {
-  if (event.deltaY > 0) {
-    nextSlide();
-  } else {
-    prevSlide();
-  }
-});
-
-setInterval(nextSlide, 5000); // Auto slide every 5 seconds
