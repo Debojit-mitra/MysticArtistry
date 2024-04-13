29ccfdc09027e6aad5ea1bdfdfc2abb714736477
Debojit Mitra <debojit16mitra@gmail.com>
2024-04-13 13:12:22 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ function openShopPage() {
   // Open showPage.html in a new tab or window
   window.open("index.html", "_self");
 }
+
+function toggleMenu() {
+  var menuItems = document.querySelectorAll("li a");
+  var checkbox = document.getElementById("click");
+  var isChecked = checkbox.checked;
+
+  if (isChecked) {
+    menuItems.forEach(function (item, index) {
+      item.classList.add("show-animation");
+    });
+  } else {
+    menuItems.forEach(function (item, index) {
+      item.classList.remove("show-animation");
+    });
+  }
+}
