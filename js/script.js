f272396156d4173617f271c99931a6a9486359a9
Debojit Mitra <debojit16mitra@gmail.com>
2024-05-04 10:09:53 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -8,36 +8,12 @@ function openShopPage() {
   window.open("../pages/shop.html", "_self");
 }
 
+function openProduct1() {
+  // Open shop.html in a new tab or window
+  window.open("../pages/products/product1.html", "_self");
+}
+
 function redirectToPage(pageUrl) {
   window.location.href = pageUrl;
 }
 
-
-//for quantity
-$(document).ready(function() {
-  $('.num-in span').click(function () {
-      var $input = $(this).parents('.num-block').find('input.in-num');
-    if($(this).hasClass('minus')) {
-      var count = parseFloat($input.val()) - 1;
-      count = count < 1 ? 1 : count;
-      if (count < 2) {
-        $(this).addClass('dis');
-      }
-      else {
-        $(this).removeClass('dis');
-      }
-      $input.val(count);
-    }
-    else {
-      var count = parseFloat($input.val()) + 1
-      $input.val(count);
-      if (count > 1) {
-        $(this).parents('.num-block').find(('.minus')).removeClass('dis');
-      }
-    }
-    
-    $input.change();
-    return false;
-  });
-  
-});
